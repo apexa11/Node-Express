4eb01f84dbdf2a235b9d2bf81d37445cc845d811
Apexa11 <apexa.k.patel@nasa.gov>
2019-04-09 14:07:45 -0400

    adding mustache templating engine for render html

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 /** import express  */
 const express = require('express')
+/** import path module */
+const path = require('path') 
 
 /** add express app */ 
 const app = express()
+
+/** set view directory path */
+app.set('views', path.join(__dirname,'views'))
+/** set mustache templating engine */
+app.set('view engine', 'mustache')
+app.engine('mustache', require('hogan-middleware').__express)
 
 /** add routes */
 app.get('/',(req, res, next) => {
@@ -14,6 +22,10 @@ app.get('/json',(req, res, next) => {
         greeting: 'Hello'
     }
     res.json(data)
+})
+
+app.get('/home',(req, res, next) => {
+    res.render('home',null)
 })
 
 app.listen(3000)
